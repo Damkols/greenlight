d92329150335ef8f60e0738c8956b9ba1db080ef
damkols <damolausmankolapo@gmail.com>
2025-06-05 15:26:02 +0100

    strings import

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -7,6 +7,7 @@ import(
 	"encoding/json"
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -75,8 +76,8 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 		case errors.Is(err, io.EOF):
 			return errors.New("body must not be empty")
 
-		case strings.HasPrefix(err.Error(), "json: unknown field "):
-			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+		case strings.HasPrefix(err.Error(),"json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(),"json: unknown field ")
 			return fmt.Errorf("body contains unknown key %s", fieldName)
 		
 		case errors.As(err, &maxBytesError):
